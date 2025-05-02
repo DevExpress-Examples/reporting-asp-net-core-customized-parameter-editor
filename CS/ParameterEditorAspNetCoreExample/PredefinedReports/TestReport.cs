1ae5ed2f32b7d1ad68427040e1f3d351872eb590
Aleksandr Korytov <aleksandr.korytov@devexpress.com>
2025-05-02 15:30:22 +0400

    VSREPX to CS/VB conversion

--- CS/ParameterEditorAspNetCoreExample/PredefinedReports/TestReport.cs
+++ CS/ParameterEditorAspNetCoreExample/PredefinedReports/TestReport.cs
@@ -1,19 +1,14 @@
-using System;
+﻿using System;
 using System.Drawing;
-using DevExpress.XtraReports.Parameters;
 using DevExpress.XtraReports.UI;
 
-namespace ParameterEditorAspNetCoreExample.PredefinedReports
-{
-    public partial class TestReport
-    {
-        public TestReport()
-        {
+namespace ParameterEditorAspNetCoreExample.PredefinedReports {
+    public partial class TestReport : DevExpress.XtraReports.UI.XtraReport {
+        public TestReport() {
             InitializeComponent();
         }
 
-        private void tableCell4_BeforePrint(object sender, System.ComponentModel.CancelEventArgs e)
-        {
+        private void tableCell4_BeforePrint(object sender, System.ComponentModel.CancelEventArgs e) {
             (sender as XRTableCell).Text += " Customized";
         }
     }
